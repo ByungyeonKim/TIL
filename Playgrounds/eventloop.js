ee7460806379ffbd5aaa20e67cb8f4c4562ebfff
ByungyeonKim <viva_kby@naver.com>
2021-05-03 18:33:17 +0900

    이벤트 루프 연습

---
+++ Playgrounds/eventloop.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const foo = () => console.log('First');
+const bar = () => setTimeout(() => console.log('Second'), 500);
+const baz = () => console.log('Third');
+
+bar();
+foo();
+baz();
+
+// First
+// Third
+// Second
