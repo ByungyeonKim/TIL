95d966e3e5b5aac00fcbb9d7d1ff47258fdd3c9a
ByungyeonKim <viva_kby@naver.com>
2021-06-04 22:35:16 +0900

    Update arr.js

--- Playgrounds/arr.js
+++ Playgrounds/arr.js
@@ -1,12 +1,17 @@
 const arr1 = [1, 2, 3, 3, 3, 3, 4, 4];
 const arr2 = [3, 2, 4, 4, 2, 5, 2, 5, 5];
 const arr3 = [3, 5, 7, 9, 1];
-
+const arr = ['a', 'b', 'a', 'b', 'c'];
 function solution(arr) {
   const map = arr.reduce((prev, curr) => {
     prev.set(curr, (prev.get(curr) || 0) + 1);
     return prev;
   }, new Map());
+
+  // const res = {};
+  // arr.forEach((x) => {
+  //   res[x] = (res[x] || 0) + 1;
+  // });
 
   result = [];
   for (let [key, val] of map) {
@@ -15,7 +20,8 @@ function solution(arr) {
   if (result.length === 0) {
     return [-1];
   }
+
   return result;
 }
 
-console.log(solution(arr3));
+console.log(solution(arr1));
