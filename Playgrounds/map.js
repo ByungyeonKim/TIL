6fbfa63067e660652226234fb3176238a6fad7a3
ByungyeonKim <viva_kby@naver.com>
2021-06-04 22:33:13 +0900

    Create map.js

--- Playgrounds/map.js
+++ Playgrounds/map.js
@@ -1,19 +1,30 @@
-const array = [1, 2, 3, 4, 5];
+const map = new Map();
 
-const map1 = array.map((x) => x);
-const map2 = array.map((x) => ({ x, plus: x + 1 }));
-const map3 = array.map((x) => {
-  if (x > 3) return x * 2;
-});
+map.set('1', 'string key');
+map.set(1, 'number key');
+map.set(true, 'boolean key');
 
-function foo(b) {
-  let a = 10;
-  return a + b;
+// 객체의 키는 반드시 String or Symbol이어야 한다.
+// 맵은 키의 타입을 그대로 유지한다.
+
+console.log(map.get(1));
+console.log(map.get('1'));
+console.log(map.get(true));
+
+const iterable = new Map([
+  [1, 5],
+  [2, 3],
+  [3, 1],
+  [6, 1],
+  [8, 2],
+]);
+
+console.log(iterable);
+console.log(iterable.get(8));
+const res = [];
+for (let [key, val] of iterable) {
+  if (val > 1) {
+    res.push(val);
+  }
 }
-
-function bar(x) {
-  let y = 2;
-  return foo(x + y);
-}
-
-console.log(bar(1));
+console.log(res);
