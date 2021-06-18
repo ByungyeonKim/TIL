f983ff60bb32cf8be825ee078029cc387230506d
ByungyeonKim <viva_kby@naver.com>
2021-06-18 22:58:47 +0900

    Fix: comments

--- Playgrounds/obj_location.js
+++ Playgrounds/obj_location.js
@@ -13,8 +13,8 @@ const keyPad = {
   '#': [2, 0],
 };
 
-// 객체 접근은 keyPad[key][value]
-// [value]가 배열일 경우 인덱스로 값 가져오기
+// 객체 접근은 keyPad[key]로 value를 불러올 수 있다.
+// value가 배열일 경우 keyPad[key][index]로 값 가져오기
 
 console.log(keyPad[5][1]); // 5번 키패드의 1번째 인덱스 = 2
 console.log(keyPad[8][0]); // 8번 키패드의 0번째 인덱스 = 1
