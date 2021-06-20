c400a163c402d398a34bc4b6c8871e371fe43ffa
ByungyeonKim <viva_kby@naver.com>
2021-06-20 16:07:27 +0900

    Add: es6

---
+++ Playgrounds/es6.js
@@ -0,0 +1,75 @@
+// Module
+// 앱을 구성하는 '개별적 요소'로써 재사용 가능한 코드 조각
+// 세부 사항을 캡슐화하고 공개가 필요한 API만을 외부에 노출
+
+// module.js의 API를 노출/불러오기
+// 모듈을 사용할 땐 HTML 스크립트에 type="module" 속성을 넣어준다.
+import { greeting, sum } from './module.js';
+
+console.log(sum(1, 6));
+console.log(greeting);
+
+// Class
+// 같은 종류의 집단에 속하는 속성(attribute)과 행위(behavior)를 정의한 것
+// 객체지향 프로그램의 기본적인 사용자 정의 데이터타입이라고 할 수 있다.
+// new 연산자를 통한 인스턴스화 과정이 필요.
+
+// 클래스 선언
+class Person {
+  // 생성자
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  sayHello() {
+    console.log(`Hello! I'm ${this.name}.`);
+  }
+}
+
+// 인스턴스 생성
+const me = new Person('Kim');
+me.sayHello(); // Hello! I'm Kim.
+
+console.log(me instanceof Person); // true
+
+// 클래스도 let, const 키워드와 같은 방식으로 호이스팅이 된다.
+{
+  // Uncaught ReferenceError: Cannot access 'Foo' before initialization
+  console.log(Foo);
+
+  class Foo {}
+}
+
+const Foo = '';
+
+{
+  // 호이스팅이 발생하지 않는다면 ''가 출력되어야 한다.
+  console.log(Foo);
+  // Uncaught ReferenceError: Cannot access 'Foo' before initialization
+  class Foo {}
+}
+
+// let, const
+// var와 달리 블록스코프 지원, 재선언, 재할당 불가 등의 차이가 있다.
+// let과 const와의 가장 큰 차이는 let은 재할당이 가능하지만 const는 상수 변수로써 재할당이 불가.
+
+// 기본값 매개변수(default parameter)
+// 값이 없거나 undefined가 전달될 경우 정해준 기본값으로 전달이 가능해졌다.
+
+// 기존에 사용하던(ES6 전) 방식
+function multiply(a, b) {
+  if (b === undefined) b = 1;
+  return a * b;
+}
+
+console.log(multiply(5)); // 5
+console.log(multiply(5, 5)); // 25
+
+// ES6 방식
+function multiply(a, b = 1) {
+  return a * b;
+}
+
+console.log(multiply(5)); // 5
+console.log(multiply(5, 5)); // 25
---
+++ Playgrounds/module.js
@@ -0,0 +1,6 @@
+const greeting = 'Hello World';
+function sum(a, b) {
+  return a + b;
+}
+
+export { greeting, sum };
