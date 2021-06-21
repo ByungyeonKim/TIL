1868ccfd4ca5b6c9daed56c4f43f3194a82dc1cd
ByungyeonKim <viva_kby@naver.com>
2021-06-22 00:22:53 +0900

    Add: sum example

--- Playgrounds/sum.js
+++ Playgrounds/sum.js
@@ -1,10 +1,16 @@
 let sum = 0;
 let count = 1;
 
-while (count < 100) {
-  sum = sum + count;
-  count++;
-  console.log(sum, count);
+// while (count < 100) {
+//   sum = sum + count;
+//   count++;
+//   console.log(sum, count);
+// }
+
+// console.log(sum);
+
+for (let i = 1; i <= 10; i++) {
+  sum = sum + i;
 }
 
 console.log(sum);
