a50950f99a973734c17864f8631c03b539d3a98f
ByungyeonKim <viva_kby@naver.com>
2021-07-31 16:00:08 +0900

    Remove comment App.jsx

--- Playgrounds/React.js/test/src/App.jsx
+++ Playgrounds/React.js/test/src/App.jsx
@@ -7,38 +7,6 @@ import ParticlesBg from 'particles-bg';
 import MouseParticles from 'react-mouse-particles';
 
 function App() {
-  // Custom
-  // let config = {
-  //   num: [3, 5],
-  //   rps: 0.1,
-  //   radius: [5, 35],
-  //   life: [1.5, 3],
-  //   v: [2, 3],
-  //   tha: [-40, 40],
-  //   alpha: [0.6, 0],
-  //   scale: [0.1, 0.4],
-  //   position: 'all',
-  //   color: ['random', '#ff0000'],
-  //   cross: 'dead',
-  //   random: 10,
-  // };
-
-  // if (Math.random() > 0.85) {
-  //   config = Object.assign(config, {
-  //     onParticleUpdate: (ctx, particle) => {
-  //       ctx.beginPath();
-  //       ctx.rect(
-  //         particle.p.x,
-  //         particle.p.y,
-  //         particle.radius * 2,
-  //         particle.radius * 2
-  //       );
-  //       ctx.fillStyle = particle.color;
-  //       ctx.fill();
-  //       ctx.closePath();
-  //     },
-  //   });
-  // }
   return (
     <BrowserRouter>
       <MouseParticles g={1} color='random' cull='col,image-wrapper' />
