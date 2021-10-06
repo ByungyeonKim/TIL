27ac87ea804e55d69ff9507ab92e3d7cc646c757
ByungyeonKim <viva_kby@naver.com>
2021-10-06 20:10:09 +0900

    Update: es6_iter.js

--- Playgrounds/es6_iter.js
+++ Playgrounds/es6_iter.js
@@ -12,37 +12,71 @@ const log = console.log;
 // }
 
 // Array를 통해 알아보기
-log('🌱 Arr-------------');
-const arr = [1, 2, 3];
-for (const a of arr) {
-  log(a);
-}
-// Set을 통해 알아보기
-log('🌿 Set-------------');
-const set = new Set([1, 2, 3]);
-for (const a of set) {
-  log(a);
-}
-// Map을 통해 알아보기
-log('🌴 Map-------------');
-const map = new Map([
-  ['a', 1],
-  ['b', 2],
-  ['c', 3],
-]);
-for (const a of map.keys()) {
-  log(a);
-}
-for (const a of map.values()) {
-  log(a);
-}
-for (const a of map.entries()) {
-  log(a);
-}
-// Array, Set, Map 모두 이터레이터 프로토콜을 따른다.
-let iterator = map[Symbol.iterator]();
-log(iterator);
-log(iterator.next());
-log(iterator.next());
-log(iterator.next());
-log(iterator.next());
+// log('🌱 Arr-------------');
+// const arr = [1, 2, 3];
+// for (const a of arr) {
+//   log(a);
+// }
+// // Set을 통해 알아보기
+// log('🌿 Set-------------');
+// const set = new Set([1, 2, 3]);
+// for (const a of set) {
+//   log(a);
+// }
+// // Map을 통해 알아보기
+// log('🌴 Map-------------');
+// const map = new Map([
+//   ['a', 1],
+//   ['b', 2],
+//   ['c', 3],
+// ]);
+// for (const a of map.keys()) {
+//   log(a);
+// }
+// for (const a of map.values()) {
+//   log(a);
+// }
+// for (const a of map.entries()) {
+//   log(a);
+// }
+// // Array, Set, Map 모두 이터레이터 프로토콜을 따른다.
+// let iterator = map[Symbol.iterator]();
+// log(iterator);
+// log(iterator.next());
+// log(iterator.next());
+// log(iterator.next());
+// log(iterator.next());
+
+// 이터러블/이터레이터 프로토콜
+// - 이터러블: 이터레이터를 리턴하는 [Symbol.iterator]를 가진 값
+// - 이터레이터: { value, done } 객체를 리턴하는 next()를 가진 값
+// - 이터러블/이터레이터 프로토콜: 이터러블을 for...of, 전개 연산자 등과 함께 동작하도록한 규약
+
+// 사용자 정의 이터러블을 통해 이해하기
+const iterable = {
+  [Symbol.iterator]() {
+    let i = 3;
+    return {
+      next() {
+        return i == 0 ? { done: true } : { value: i--, done: false };
+      },
+      [Symbol.iterator]() {
+        return this;
+      },
+    };
+  },
+};
+
+let iterator = iterable[Symbol.iterator]();
+// log(iterator.next());
+// iterator.next();
+// iterator.next();
+// iterator.next();
+// iterator.next();
+// iterator.next();
+
+// for (const a of iterator) log(a);
+const all = document.querySelectorAll('*');
+for (const a of all) log(a.nodeName);
+log(all);
+log(all[Symbol.iterator]().next());
