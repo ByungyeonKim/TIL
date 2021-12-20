ae728ae86e04e7ce0f1b175e56d6e4db95ab9466
ByungyeonKim <viva_kby@naver.com>
2021-12-20 17:13:10 +0900

    SEO 최적화, 상세페이지 SSR 적용

--- Playgrounds/React.js/next_app/pages/detail/[id].js
+++ Playgrounds/React.js/next_app/pages/detail/[id].js
@@ -1,40 +1,34 @@
+import Head from 'next/head';
 import axios from 'axios';
-import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
 import Detail from '../../src/components/detail/detail';
-import { Loader } from 'semantic-ui-react';
 
-const Item = () => {
-  const router = useRouter();
-  const { id } = router.query;
-  const [item, setItem] = useState({});
-  const [isLoading, setLoading] = useState(true);
-
-  const url = `http://makeup-api.herokuapp.com/api/v1/products/${id}.json`;
-  const getData = async () => {
-    const res = await axios.get(url);
-    const result = setItem(res.data);
-    setLoading(false);
-    return result;
-  };
-  useEffect(() => {
-    if (id && id > 0) {
-      getData();
-    }
-  }, [id]);
+const Item = ({ item }) => {
   return (
     <>
-      {isLoading ? (
-        <div style={{ minHeight: '100vh', paddingTop: '20rem' }}>
-          <Loader inline='centered' active>
-            Loading
-          </Loader>
-        </div>
-      ) : (
-        <Detail item={item} />
+      {item && (
+        <>
+          <Head>
+            <title>{item.name}</title>
+            <meta name='description' content={item.description}></meta>
+          </Head>
+          <Detail item={item} />
+        </>
       )}
     </>
   );
 };
 
 export default Item;
+
+export async function getServerSideProps(context) {
+  const id = context.params.id;
+  const url = `http://makeup-api.herokuapp.com/api/v1/products/${id}.json`;
+  const res = await axios.get(url);
+  const result = res.data;
+
+  return {
+    props: {
+      item: result,
+    },
+  };
+}
