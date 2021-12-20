0b9b86a2b258bfbfefd3d35c7c83c224f033c8d3
ByungyeonKim <viva_kby@naver.com>
2021-12-20 12:04:08 +0900

    동적 라우트, 상세 페이지 및 스타일 추가

--- Playgrounds/React.js/next_app/pages/view/[id].js
+++ Playgrounds/React.js/next_app/pages/view/[id].js
@@ -1,3 +1,25 @@
-export default function id(params) {
-  return <h1>ID</h1>;
-}
+import axios from 'axios';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import Detail from '../../src/components/detail/detail';
+
+const Item = () => {
+  const router = useRouter();
+  const { id } = router.query;
+  const [item, setItem] = useState({});
+
+  const url = `http://makeup-api.herokuapp.com/api/v1/products/${id}.json`;
+  const getData = async () => {
+    const res = await axios.get(url);
+    const result = setItem(res.data);
+    return result;
+  };
+  useEffect(() => {
+    if (id && id > 0) {
+      getData();
+    }
+  }, [id]);
+  return <Detail item={item} />;
+};
+
+export default Item;
--- Playgrounds/React.js/next_app/src/components/itemList/itemList.js
+++ Playgrounds/React.js/next_app/src/components/itemList/itemList.js
@@ -1,5 +1,6 @@
-import { Divider, Grid, Image } from 'semantic-ui-react';
+import { Grid, Image } from 'semantic-ui-react';
 import styles from './itemList.module.css';
+import Link from 'next/link';
 
 const ItemList = ({ list }) => {
   return (
@@ -7,23 +8,25 @@ const ItemList = ({ list }) => {
       <Grid columns='three'>
         <Grid.Row className={styles.item_row}>
           {list.map((item) => (
-            <Grid.Column key={item.id}>
-              <div className={styles.item_container}>
-                <Image
-                  src={item.image_link}
-                  alt={item.name}
-                  className={styles.item_img}
-                />
-                <h4 className={styles.item_name}>{item.name}</h4>
-                <p>
-                  {item.category
-                    ? `${item.category} | ${item.product_type}`
-                    : item.product_type}
-                </p>
-                <strong
-                  className={styles.item_price}
-                >{`$${item.price}`}</strong>
-              </div>
+            <Grid.Column key={item.id} className={styles.item_link}>
+              <Link href={`/view/${item.id}`}>
+                <div className={styles.item_container}>
+                  <Image
+                    src={item.image_link}
+                    alt={item.name}
+                    className={styles.item_img}
+                  />
+                  <h4 className={styles.item_name}>{item.name}</h4>
+                  <p>
+                    {item.category
+                      ? `${item.category} | ${item.product_type}`
+                      : item.product_type}
+                  </p>
+                  <strong
+                    className={styles.item_price}
+                  >{`$${item.price}`}</strong>
+                </div>
+              </Link>
             </Grid.Column>
           ))}
         </Grid.Row>
